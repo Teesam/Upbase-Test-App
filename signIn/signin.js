8ffc698bae8747d15865dc9addc3d5e825c0e3f8
Olayiwola Samuel Tunde <50628320+Teesam@users.noreply.github.com>
2020-07-22 03:39:54 +0100

    Add files via upload

---
+++ signIn/signin.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './signin.css';
+import Nav from '../home/nav/nav';
+import Footer from '../home/footer/footer';
+import { Link } from 'react-router-dom';
+
+
+const SignIn = () => {
+
+    const  [username, setUsername] = useState('');
+
+    const usernameHandler = e => {
+        setUsername( e.target.value );
+    }
+
+    console.log(username); 
+
+    return(
+        <div className = 'SignIn'>
+            <Nav />
+                <form id = 'signIn-form'>
+                    <h2 id = 'signIn-header'>Sign-In</h2>
+                    <div className = 'SignIn-header-div'>
+                        <label htmlFor = 'username'>Username</label>
+                        <input onChange ={usernameHandler} name = 'username' type = 'text' id = 'signIn-input' />
+                    </div>
+                    <Link to = '/signInPassword' className = 'SignIn-btn'>Next</Link>
+
+                    <p id = 'register-cue'>Don't have an account? <Link to = '/register' id = 'cue-link'>Register</Link></p>
+                </form>
+            <Footer />
+        </div>
+    )
+}
+
+export default SignIn;
